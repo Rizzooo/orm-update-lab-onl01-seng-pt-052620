71f98d22d6b02dfea2c24e26da43eca0bd9f2539
Ronald Burrell Jr <ronburrell05@gmail.com>
2020-07-27 21:59:44 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -12,7 +12,11 @@ class Student
   
   def self.create_table
     sql = <<-SQL
-      CREATE TABLE IF NOT EXISTS
+      CREATE TABLE IF NOT EXISTS songs (
+      id INTEGER PRIMARY KEY, 
+      name TEXT, 
+      grade INTEGER
+      )
     SQL
   end
 
