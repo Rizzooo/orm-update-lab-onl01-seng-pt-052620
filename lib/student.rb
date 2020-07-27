d2fa66893b086e64d20735aa35c5bc9ae8fa348c
Ronald Burrell Jr <ronburrell05@gmail.com>
2020-07-27 22:19:03 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -48,7 +48,7 @@ class Student
     student
   end
   
-  def self.new_from_db(name)
+  def self.new_from_db(id)
     new_student = Student.new(name, grade, id)
     new_student.save
     new_student
